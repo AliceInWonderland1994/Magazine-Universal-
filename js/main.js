6919a6f7525cebb78b878abce474754bb7da319f
Alise <alicemint@mail.ru>
2020-10-27 15:32:08 +0400

    create: added video player

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    const categoriesTab = $('.categories__tab');
+    const media = $('.media');
+
+    categoriesTab.on('click', function (event) {
+        const activeTab = $(this).attr('data-target');
+
+        categoriesTab.removeClass('categories__tab--active')
+        media.removeClass('media--active');
+        $(activeTab).addClass('media--active');
+        $(this).addClass('categories__tab--active');
+    });
+});
+
+const buttonPlay = document.querySelector('.buttons-player__play');
+const buttonPlayNfl = document.querySelector('.buttons-player__play-2');
+const video = document.querySelector('.video');
+const videoNfl = document.querySelector('.video-nfl');
+
+const videoPlay = function (button, video) {
+    button.addEventListener('click', function (evt) {
+        evt.preventDefault();
+
+        video.classList.toggle('video--active');
+    });
+};
+videoPlay(buttonPlay, video);
+videoPlay(buttonPlayNfl, videoNfl);
